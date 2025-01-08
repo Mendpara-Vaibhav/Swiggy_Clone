4ffc332a11c5707bd13c2df26635b9e1edba9229
Mendpara Vaibhav <mendparavaibhav@gmail.com>
2025-01-08 16:44:14 +0530

    Ep-06 fullfill

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,38 +1,83 @@
 import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
-import resList from "../utils/mockData";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  // Local State Variable - Super powerful variable
+  // Whenever state variable updates, react triggers a reconciliation cycle(re-renders the component)
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  const [searchText, setSearchText] = useState("");
 
-  // const fetchData = async () => {
-  //   const data = await fetch(
-  //     "https://www.swiggy.com/dapi/restaurants/search/v3?lat=23.022505&lng=72.5713621&str=Ahmedabad%20Restaurants&trackingId=undefined&submitAction=ENTER&queryUniqueId=f27bd1cd-7f65-84b4-a015-eb0d98a77f37"
-  //   );
-  //   const json = await data.json();
-  //   console.log(json);
-  // };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/search/v3?lat=23.022505&lng=72.5713621&str=Ahmedabad%20Restaurants&trackingId=undefined&submitAction=ENTER&queryUniqueId=f27bd1cd-7f65-84b4-a015-eb0d98a77f37"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+
+    //Optional Chaining
+    setListOfRestaurants(
+      json?.data?.cards[1]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[1]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards
+    );
+  };
 
   // Conditional Rendering
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer />;
-  }
+  // if (listOfRestaurants.length === 0) {
+  //   return <Shimmer />;
+  // }
+  // we can write it in the return  statement as bottom
 
-  return (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              //Filter the restaurant cards and update the UI
+              // searchText
+              console.log(searchText);
+
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.card.card.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
               (res) => res.card.card.info.avgRating > 4.2
             );
+
             setListOfRestaurants(filteredList);
           }}
         >
@@ -40,7 +85,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard
             key={restaurant.card.card.info.id}
             resData={restaurant}
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,10 +1,25 @@
 const Shimmer = () => {
   return (
-    <div className="shimmer-container">
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-    </div>
+    <>
+      <div>
+        <button className="shimmer-btn">_____________</button>
+        <button className="shimmer-btn">_____________</button>
+      </div>
+      <div className="shimmer-container">
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+      </div>
+    </>
   );
 };
 
